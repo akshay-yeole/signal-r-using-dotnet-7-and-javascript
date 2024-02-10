a02ee77446384c33b78b5f4d1c47c2634bc945b1
akshay-yeole <akshayyeole1@outlook.com>
2024-02-10 18:34:13 +0530

    Add project files.

---
+++ signal-r-using-dotnet-7/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace signal_r_using_dotnet_7.Hubs
+{
+    public class ChatHub :  Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
